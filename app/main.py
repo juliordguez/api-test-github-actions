43c186bea536f7a58b28f58954ecf8f726fc3ba8
Julio rodriguez <jcro.chris@gmail.com>
2025-12-02 09:56:19 -0600

    Actualizacion de rutas

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from typing import List, Dict
-from app.models import models
+from .models.models import Item, ItemBase
 
 
 app = FastAPI(title="API CRUD básica con FastAPI")
 
 
-db: Dict[int, models.Item] = {}
+db: Dict[int, Item] = {}
 next_id: int = 1
 
 
@@ -15,32 +15,32 @@ def root():
     return {"message": "API CRUD básica con FastAPI"}
 
 
-@app.get("/items", response_model=List[models.Item])
+@app.get("/items", response_model=List[Item])
 def list_items():
     return list(db.values())
 
 
-@app.get("/items/{item_id}", response_model=models.Item)
+@app.get("/items/{item_id}", response_model=Item)
 def get_item(item_id: int):
     if item_id not in db:
         raise HTTPException(status_code=404, detail="Item no encontrado")
     return db[item_id]
 
 
-@app.post("/items", response_model=models.Item, status_code=201)
-def create_item(item: models.ItemBase):
+@app.post("/items", response_model=Item, status_code=201)
+def create_item(item: ItemBase):
     global next_id
-    new_item = models.Item(id=next_id, **item.dict())
+    new_item = Item(id=next_id, **item.dict())
     db[next_id] = new_item
     next_id += 1
     return new_item
 
 
-@app.put("/items/{item_id}", response_model=models.Item)
-def update_item(item_id: int, item: models.ItemBase):
+@app.put("/items/{item_id}", response_model=Item)
+def update_item(item_id: int, item: ItemBase):
     if item_id not in db:
         raise HTTPException(status_code=404, detail="Item no encontrado")
-    updated = models.Item(id=item_id, **item.dict())
+    updated = Item(id=item_id, **item.dict())
     db[item_id] = updated
     return updated
 
